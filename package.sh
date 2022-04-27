29a612eb4f0dbf3a04eb3bc77178c3e3639f665c
Gildas Lebel <gildaslebel@gmail.com>
2022-04-26 20:31:41 -0400

    Make sure the Unit test are executed before deploying

--- package.sh
+++ package.sh
@@ -1,7 +1,12 @@
-pip install --target ./package -r requirements.txt
+#!/bin/bash
+pip install --target ./dependencies -r requirements.txt
 
 zip -r mindmaprest-package.zip mindmap_api *.py
 
-cd package/
+# Dependencies need to be in the root of the zip file
+cd dependencies/
 zip -ur ../mindmaprest-package.zip *
 cd -
+
+# Exit with success code
+exit 0
